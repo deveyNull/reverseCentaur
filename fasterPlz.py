55632d93c6383c297f31e9454c186f5b3cf45704
Dennis Devey <d.m.devey@gmail.com>
2017-11-16 23:21:19 -0500

    Update fasterPlz.py

--- fasterPlz.py
+++ fasterPlz.py
@@ -475,7 +475,32 @@ def enrichHTTP(dictEntry, domainName):
         ))
         
     finalArray = list(itertools.chain.from_iterable(finalArray))
+    # domainName, 
+# count, count # just in general useful for all of this... if you use total values for things like bytes or packets io, should be used to scale results.
 
+# Word Magic: return([countUnique, percentageUnique, modeCount, percentageMode]) 
+
+# temp0, subdomain array #super important for DNS, less so for http
+# temp1, agent array #unlikely, ignore
+# temp2, uri array #super important for http, encoded in URI
+
+# Math Magic: (return([countUnique, percentageUnique, average, minimum, maximum, entStat, varStat, skewStat,kurtosisStat])
+
+
+# temp_0, delta time list # very important, periodicity?
+# magicDurationArray, durations #possibly important
+# magicOrigBytesArray, bytes sent #yes * maybe something can be done with ratios here
+# magicRespBytesArray, bytes received #yes
+# magicOrigPacketsArray, packets sent #yes
+# magicOrigIpBytesArray, ip bytes sent #yes
+# magicRespPacketsArray, packets recieved #yes
+# magicRespIpBytesArray, ip bytes recieved #yes *  maybe something can be done with ratios here
+# temp_2, uri length
+# temp_3, uri depth
+# temp_4, uri entropy 
+# temp_5, agent length #unlikely to matter, #unlikely to matter
+# temp_6, agent depth #unlikely to matter, #unlikely to matter
+# temp_7, agent entropy #unlikely to matter, recommend ignore
     return(finalArray)
     
 def listMaker(csvOne):
